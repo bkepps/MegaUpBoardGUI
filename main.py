22b9d29a292852666e634044a06d0095dae84034
bkepps <bk970@protonmail.com>
2021-11-30 14:47:55 -0500

    made elements more customizable

--- main.py
+++ main.py
@@ -10,29 +10,13 @@ from functools import partial
 def prnt(messag, instance):
     print(messag)
         
+class Quiz(Widget):
+    pass
+
 class MyGUI(App):
 
     def build(self):
-        wid = Widget()
-
-        #label = Label(text='0')
-        buttonA = Button(text='A', on_press=partial(prnt, 'A'))
-        buttonB = Button(text='B', on_press=partial(prnt, 'B'))
-        buttonC = Button(text='C', on_press=partial(prnt, 'C'))
-        buttonD = Button(text='D', on_press=partial(prnt, 'D'))
-
-        layout = BoxLayout(size_hint=(1, None), height=50)
-        #layout.add_widget(label)
-        layout.add_widget(buttonA)
-        layout.add_widget(buttonB)
-        layout.add_widget(buttonC)
-        layout.add_widget(buttonD)
-
-        root = BoxLayout(orientation='vertical')
-        root.add_widget(wid)
-        root.add_widget(layout)
-
-        return root
+        return Quiz()
         
 if __name__ == '__main__':
     MyGUI().run()
