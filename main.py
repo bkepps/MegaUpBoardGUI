16b4c7aa3473f9a481b3a8e16d1fc601e02e536e
bkepps <bk970@protonmail.com>
2021-11-30 14:02:36 -0500

    four multiple choice buttons, and they print which button to cmd when clicked

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ from kivy.graphics import Color, Rectangle
 from random import random as r
 from functools import partial
 
-def callback(instance):
-    print('yeet')
+def prnt(messag, instance):
+    print(messag)
         
 class MyGUI(App):
 
     def build(self):
         wid = Widget()
 
-        label = Label(text='0')
-        button = Button(text='Hello world', font_size=14)
-        button.bind(on_press=callback)
+        #label = Label(text='0')
+        buttonA = Button(text='A', on_press=partial(prnt, 'A'))
+        buttonB = Button(text='B', on_press=partial(prnt, 'B'))
+        buttonC = Button(text='C', on_press=partial(prnt, 'C'))
+        buttonD = Button(text='D', on_press=partial(prnt, 'D'))
 
         layout = BoxLayout(size_hint=(1, None), height=50)
-        layout.add_widget(label)
-        layout.add_widget(button)
+        #layout.add_widget(label)
+        layout.add_widget(buttonA)
+        layout.add_widget(buttonB)
+        layout.add_widget(buttonC)
+        layout.add_widget(buttonD)
 
         root = BoxLayout(orientation='vertical')
         root.add_widget(wid)
